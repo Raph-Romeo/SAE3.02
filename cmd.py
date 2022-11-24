79aaa4ea7975b95b9a7dfd03e9c1ae03b10483a2
Raph-Romeo <95073137+Raph-Romeo@users.noreply.github.com>
2022-11-24 22:00:38 +0100

    Update cmd.py

--- cmd.py
+++ cmd.py
@@ -7,34 +7,61 @@ import os
 def windows_command(cmd_in: str) -> str:
     try:
         cmd = cmd_in.lower()
+        if len(cmd.split(' ')) > 1:
+            cmd = cmd.split(' ')[0]
+            args = cmd_in.split(' ', 1)[1].lower()
+        else:
+            args = None
         if cmd == 'os':
             return 'Operating System: Windows 10\nMachine name: ' + gethostname()
         elif cmd == 'ip':
-            return str(subprocess.check_output('ipconfig', shell=True)).replace('\\r\\n','\n').split("'",1)[1].split("'",1)[0]
-        elif cmd == 'ip -a':
-            return str(subprocess.check_output('ipconfig /all', shell=True)).replace('\\r\\n','\n').split("'",1)[1].split("'",1)[0]
+            if args is None:
+                return str(subprocess.check_output('ipconfig', shell=True)).replace('\\r\\n','\n').split("'",1)[1].split("'",1)[0]
+            elif args == '-a':
+                return str(subprocess.check_output('ipconfig /all', shell=True)).replace('\\r\\n','\n').split("'",1)[1].split("'",1)[0]
+            else:
+                return str('unrecognized argument : ' + cmd_in.split(' ', 1)[1])
         elif cmd == 'ram':
             return os.system('')
-        elif cmd == 'disconnect' or cmd == 'exit' or cmd == 'quit':
+        elif cmd == 'disconnect' or cmd == 'exit' or cmd == 'quit' or cmd == 'leave':
             return 'closing socket'
         elif cmd == 'kill':
             return 'closing socket'
         elif cmd == 'reset':
             return 'closing socket'
+        elif cmd == 'logs':
+            if args != None:
+                if args == 'clear':
+                    with open('logs.txt','w') as file:
+                        file.write('')
+                    return 'Server logs were cleared!'
+                else:
+                    i = int(args)
+                    if i < 1:
+                        return 'logs : value must be superior to 0!'
+            else:
+                i = 25
+            with open('logs.txt','r') as file:
+                lines = file.readlines()
+                if i > len(lines):
+                    i = len(lines)
+                    string = f'Showing all logs:\n'
+                else:
+                    string = f'Showing {i} of the last logs:\n'
+                for line in lines[len(lines)-i:len(lines)]:
+                    string += line
+            return string
         elif cmd == 'help':
             return 'List of commands:\nOS - displays operating system details\nIP - displays all ip information of the machine\nRAM - displays total memory available on the machine\nDISCONNECT -closes client connection\nKILL -closes server socket'
-        elif len(cmd.split(' ')[0]) > 1:
-            if cmd.split(' ')[0] == 'ping':
-                ip = cmd.split(' ')[1]
-                return str(subprocess.check_output('ping ' + ip, shell=True)).replace('\\r\\n','\n').split("'",1)[1].split("'",1)[0]
-            elif cmd.split(' ')[0] == 'echo':
-                return cmd_in.split(' ',1)[1]
-            else:
-                return 'Unknown command. Type help for a list of commands'
+        elif cmd == 'ping':
+            ip = args
+            return str(subprocess.check_output('ping ' + ip, shell=True)).replace('\\r\\n','\n').split("'",1)[1].split("'",1)[0]
+        elif cmd == 'echo':
+            return cmd_in.split(' ',1)[1]
         else:
-            return 'Unknown command. Type help for a list of commands'
+            return 'Command not found. Type help for a list of available commands'
     except:
-        return 'Error'
+        return 'Error when executing command: ' + cmd_in
 
 
 def cmd_run(connection, cmd:str) -> None:
@@ -45,7 +72,6 @@ def cmd_run(connection, cmd:str) -> None:
     elif sys.platform == "win32":
         data = windows_command(cmd).encode()
     connection.send(data)
-    if cmd == 'disconnect' or cmd == 'kill' or cmd == 'exit' or cmd == 'quit':
+    if cmd == 'disconnect' or cmd == 'kill' or cmd == 'exit' or cmd == 'quit' or cmd == 'leave':
         connection.close()
     sys.exit()
-
