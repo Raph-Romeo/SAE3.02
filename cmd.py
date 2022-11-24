301ebcfb85b658f7ac5e34187a043897dacf5433
Raph-Romeo <95073137+Raph-Romeo@users.noreply.github.com>
2022-11-24 13:48:18 +0100

    Update cmd.py

--- cmd.py
+++ cmd.py
@@ -4,9 +4,9 @@ from socket import gethostname
 import os
 
 
-def windows_command(cmd: str, conn) -> str:
+def windows_command(cmd_in: str) -> str:
     try:
-        cmd = cmd.lower()
+        cmd = cmd_in.lower()
         if cmd == 'os':
             return 'Operating System: Windows 10\nMachine name: ' + gethostname()
         elif cmd == 'ip':
@@ -21,11 +21,18 @@ def windows_command(cmd: str, conn) -> str:
             return 'closing socket'
         elif cmd == 'reset':
             return 'closing socket'
-        elif cmd.split(' ')[0] == 'ping':
-            ip = cmd.split(' ')[1]
-            return str(subprocess.check_output('ping ' + ip, shell=True)).replace('\\r\\n','\n').split("'",1)[1].split("'",1)[0]
+        elif cmd == 'help':
+            return 'List of commands:\nOS - displays operating system details\nIP - displays all ip information of the machine\nRAM - displays total memory available on the machine\nDISCONNECT -closes client connection\nKILL -closes server socket'
+        elif len(cmd.split(' ')[0]) > 1:
+            if cmd.split(' ')[0] == 'ping':
+                ip = cmd.split(' ')[1]
+                return str(subprocess.check_output('ping ' + ip, shell=True)).replace('\\r\\n','\n').split("'",1)[1].split("'",1)[0]
+            elif cmd.split(' ')[0] == 'echo':
+                return cmd_in.split(' ',1)[1]
+            else:
+                return 'Unknown command. Type help for a list of commands'
         else:
-            return 'Unknown command'
+            return 'Unknown command. Type help for a list of commands'
     except:
         return 'Error'
 
@@ -36,8 +43,9 @@ def cmd_run(connection, cmd:str) -> None:
     elif sys.platform == "darwin":
         print("NOT DONE YET") #MACOS
     elif sys.platform == "win32":
-        data = windows_command(cmd,connection).encode()
+        data = windows_command(cmd).encode()
     connection.send(data)
     if cmd == 'disconnect' or cmd == 'kill' or cmd == 'exit' or cmd == 'quit':
         connection.close()
     sys.exit()
+
