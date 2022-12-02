fa5f5ceda57b0884a37415e7fa3ec9c28fb64236
Raph-Romeo <95073137+Raph-Romeo@users.noreply.github.com>
2022-12-02 23:29:24 +0100

    Update cmd.py

--- cmd.py
+++ cmd.py
@@ -1,6 +1,8 @@
 import sys
 import subprocess
-from socket import gethostname
+from socket import gethostname, gethostbyname
+import psutil
+import platform
 import os
 
 
@@ -13,41 +15,54 @@ def windows_command(cmd_in: str) -> str:
         else:
             args = None
         if cmd == 'os':
-            return str(subprocess.check_output('wmic os get Caption,CSDVersion /value | findstr /B /C:"Caption="', shell=True)).split("=")[1].replace('\\r\\n','').replace("\\r'","")
+            return subprocess.check_output('wmic os get Caption,CSDVersion /value | findstr /B /C:"Caption="', shell=True).decode().split('=')[1]
         elif cmd == 'ip':
             if args is None:
-                return str(subprocess.check_output('ipconfig', shell=True)).replace('\\r\\n','\n').split("'",1)[1].split("'",1)[0]
+                return 'Local IP : ' + gethostbyname(gethostname())
             elif args == '-a':
-                return str(subprocess.check_output('ipconfig /all', shell=True)).replace('\\r\\n','\n').split("'",1)[1].split("'",1)[0]
+                return subprocess.check_output('ipconfig /all', shell=True).decode()
             elif args == '-e':
-                return str(subprocess.check_output("""powershell -C "Resolve-DNSName -Name "'myip.opendns.com'" -server "'resolver1.opendns.com'" | select IPAdress" ^| findstr "\."'' """, shell=True)).replace('\\r\\n','\n').split("'",1)[1].split("'",1)[0]
+                return subprocess.check_output("""powershell -C "Resolve-DNSName -Name "'myip.opendns.com'" -server "'resolver1.opendns.com'" | select IPAdress" ^| findstr "\."'' """, shell=True).decode()
             else:
                 return str('unrecognized argument : ' + cmd_in.split(' ', 1)[1])
         elif cmd == 'name':
             return gethostname()
         elif cmd == 'ram':
-            stdout = str(subprocess.check_output("wmic computersystem get totalphysicalmemory", shell=True)).replace('\\r\\n','').replace('\\r','')
-            memory = stdout.split(' ')[2]
-            if len(memory) >= 9:
-                memory = str(round(int(memory)/1000000000,2)) + 'GB'
+            memory = psutil.virtual_memory()
+            if len(str(memory[0])) >= 9:
+                memory_total = str(round(memory[0]/1000000000,2)) + 'GB'
             elif len(memory) >= 6:
-                memory = str(round(int(memory)/1000000,2)) + 'MB'
+                memory_total = str(round(memory[0]/1000000,2)) + 'MB'
             else:
-                memory = str(round(int(memory)/1000,2)) + 'KB'
-            available = str(subprocess.check_output('systeminfo |find "Available Physical Memory"', shell=True)).replace(',',' ')
-            return 'Total memory: ' + memory + '\n' + available.replace("b'", "").replace("\\r\\n'", "")
+                memory_total = str(round(memory[0]/1000,2)) + 'KB'
+            output = 'Total Memory: ' + memory_total + '\r\n'
+            if len(str(memory[3])) >= 9:
+                memory_used = str(round(memory[3]/1000000000,2)) + 'GB'
+            elif len(memory) >= 6:
+                memory_used = str(round(memory[3]/1000000,2)) + 'MB'
+            else:
+                memory_used = str(round(memory[3]/1000,2)) + 'KB'
+            output += 'Used Memory: ' + memory_used + '\r\n'
+            if len(str(memory[1])) >= 9:
+                memory_available = str(round(memory[1]/1000000000,2)) + 'GB'
+            elif len(memory) >= 6:
+                memory_available = str(round(memory[1]/1000000,2)) + 'MB'
+            else:
+                memory_available = str(round(memory[1]/1000,2)) + 'KB'
+            output += 'Available Memory: ' + memory_available
+            return output
         elif cmd == 'cpu':
-            powershell = "Powershell \"Get-Counter '\Processor(*)\% Processor Time' | Select -Expand Countersamples | Select InstanceName, CookedValue\""
-            stdout = str(subprocess.check_output(powershell, shell=True)).replace('\\r\\n','').replace("'"," ")
-            percent = str(round(float(stdout.split(' ')[len(stdout.split(' ')) - 2]),2))
-            stdout = f"CPU USAGE: {percent}%"
+            stdout = 'CPU: ' + str(platform.processor()) + '\r\nUSAGE: ' + str(psutil.cpu_percent()) + '%'
             return stdout
         elif cmd == 'disconnect' or cmd == 'exit' or cmd == 'quit' or cmd == 'leave':
             return 'closing socket'
         elif cmd == 'kill':
-            return 'closing socket'
+            if args is None:
+                return 'closing socket'
+            else:
+                return subprocess.check_output('taskkill /F /IM ' + args +  ' /T', shell=True).decode()
         elif cmd == 'reset':
-            return 'closing socket'
+            return 'resetting'
         elif cmd == 'logs':
             if args != None:
                 if args == 'clear':
@@ -64,42 +79,85 @@ def windows_command(cmd_in: str) -> str:
                 lines = file.readlines()
                 if i > len(lines):
                     i = len(lines)
-                    string = f'Showing all logs:\n'
+                    string = f'Showing all logs:\r\n'
                 else:
-                    string = f'Showing {i} of the last logs:\n'
+                    string = f'Showing {i} of the last logs:\r\n'
                 for line in lines[len(lines)-i:len(lines)]:
                     string += line
             return string
         elif cmd == 'help':
-            commands = ['DISCONNECT,LEAVE,QUIT,EXIT - close client connection','RESET - reset server','KILL - closes server','OS - displays operating system name','NAME - displays machine name','RAM - displays total and available memory','CPU - displays CPU percentage','LOGS [number] or [clear] - displays server logs','IP [-a] - Displays machine IP information','PING <destination> - ping a destination address','CLEAR - clears terminal','RENAME <name> - renames client','DOS:<msdos command> - Execute DOS commands','POWERSHELL:<powershell command> - Execute Powershell command']
-            string = ''
-            for i in commands:
-                string += i + '\n'
-            return 'List of commands:\n' + string
+            commands = """Available commands:
+DISCONNECT,LEAVE,QUIT,EXIT - close client connection
+RESET - reset server
+KILL - closes server OR task. KILL [taskname]
+OS - displays operating system name
+NAME - displays machine name
+RAM - displays total and available memory
+CPU - displays CPU percentage
+LOGS [number] or [clear] - displays server logs
+IP [-a] - Displays machine IP information
+PING <destination> - ping a destination address
+CLEAR - clears terminal
+RENAME <name> - renames client
+DOS:<msdos command> - Execute DOS commands
+POWERSHELL:<powershell command> - Execute Powershell command"""
+            return commands
         elif cmd == 'ping':
             ip = args
-            return str(subprocess.check_output('ping ' + ip, shell=True)).replace('\\r\\n','\n').split("'",1)[1].split("'",1)[0]
+            return subprocess.check_output('ping ' + ip, shell=True).decode()
         elif cmd[0:4] == 'dos:':
             command = cmd_in.split(':',1)[1]
-            output = str(subprocess.check_output(command, shell=True)).replace('\\r\\n','\n').split("'",1)[1].split("'",1)[0]
+            output = subprocess.check_output(command, shell=True).decode()
             if len(output) == 0:
-                output = 'OK'
+                output = 'success'
             return output
         elif cmd[0:11] == 'powershell:':
             command = cmd_in.split(':',1)[1]
-            output = str(subprocess.check_output('powershell -command "' + command + '"', shell=True)).replace('\\r\\n','\n').split("'",1)[1].split("'",1)[0]
+            output = subprocess.check_output('powershell -command "' + command + '"', shell=True).decode()
             if len(output) == 0:
-                output = 'OK'
+                output = 'success'
             return output
+        elif cmd[0:6] == 'linux:':
+            return 'This is a windows machine, try using "dos:" instead'
         else:
             return 'Command not found. Type help for a list of available commands'
     except:
         return 'Error when executing command: ' + cmd_in
 
 
+
+def linux_command(cmd_in:str) -> str:
+    try:
+        cmd = cmd_in.lower()
+        if len(cmd.split(' ')) > 1:
+            cmd = cmd.split(' ')[0]
+            args = cmd_in.split(' ', 1)[1].lower()
+        else:
+            args = None
+        if cmd == 'os':
+            return subprocess.check_output('cat /etc/os-release', shell=True).decode()
+        elif cmd == 'ip':
+            if args is None:
+                return subprocess.check_output('ip', shell=True).decode()
+            elif args == '-a':
+                return subprocess.check_output('ip a', shell=True).decode()
+            else:
+                return 'Unknown argument ' + args
+        elif cmd == 'ping':
+            if args is None:
+                return 'Failed to ping. Must specify address'
+            else:
+                return subprocess.check_output('ping -c 4' + args, shell=True).decode()
+        elif cmd[0:6] == 'linux:':
+            command = cmd_in.split(':',1)[1]
+            return subprocess.check_output(command, shell=True).decode()
+    except:
+        return 'Failed to execute command: ' + cmd_in
+
+
 def cmd_run(connection, cmd:str) -> None:
     if sys.platform == "linux" or sys.platform == "linux2":
-        print("NOT DONE YET") #LINUX
+        data = linux_command(cmd).encode()
     elif sys.platform == "darwin":
         print("NOT DONE YET") #MACOS
     elif sys.platform == "win32":
