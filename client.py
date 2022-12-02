796281f484ff4eec118e79e32cb59ea170f6c795
Raph-Romeo <95073137+Raph-Romeo@users.noreply.github.com>
2022-12-02 23:26:53 +0100

    Update client.py

--- client.py
+++ client.py
@@ -8,37 +8,67 @@ from PyQt5.QtGui import QIcon, QCursor
 from PyQt5 import Qt
 from PyQt5.QtCore import QCoreApplication, QObject, QThread, pyqtSignal
 from PyQt5 import QtCore
+import time
 from functools import partial
 
 #________
 
 class ScrollLabel(QScrollArea):
  
-    def __init__(self, *args, **kwargs):
+    def __init__(self,parent,*args, **kwargs):
         QScrollArea.__init__(self, *args, **kwargs)
         self.setWidgetResizable(True)
         content = QWidget(self)
         self.setWidget(content)
-        layout = QVBoxLayout(content)
-        self.label = QLabel(content)
-        self.label.setStyleSheet("color:white;font-family:verdana;font-size:12px;border-bottom:none;")
+        self.parent = parent
+        layout = QGridLayout(content)
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.label = QLabel('')
+        layout.setRowStretch(2, 1)
+        self.label.setStyleSheet("color:white;font-family:verdana;font-size:12px;border-bottom:none;background-color:transparent;padding:10px;padding-top:0px;padding-bottom:0px;")
         self.setStyleSheet(stylesheet)
         self.label.setWordWrap(True)
         self.label.setAlignment(Qt.Qt.AlignTop)
         #self.label.setTextInteractionFlags()
-        layout.addWidget(self.label)
+        self.cmdinput = QLineEdit('')
+        self.username = QLabel(self.parent.servername + ' >')
+        self.username.setStyleSheet("color:white;font-family:verdana;font-size:12px;border-bottom:none;background-color:transparent;padding-left:10px;padding-bottom:10px;")
+        self.cmdinput.setPlaceholderText('Type password . . .')
+        self.cmdinput.setStyleSheet('border:none;margin-top:0px;background:#000;font-family:Verdana;color:white;font-size:12px;padding-bottom:10px;')
+        self.cmdinput.editingFinished.connect(self.parent.send_message)
+        self.cmdinput.setAlignment(Qt.Qt.AlignTop)
+        self.cmdinput.setEchoMode(QLineEdit.Password)
+        self.verticalScrollBar().rangeChanged.connect(self.__scroll_to_bottom)
+        layout.addWidget(self.label,0,0,1,2)
+        layout.addWidget(self.username,1,0)
+        layout.addWidget(self.cmdinput,1,1)
+    
+    def __scroll_to_bottom(self):
+        verScrollBar = self.verticalScrollBar()
+        verScrollBar.setValue(verScrollBar.maximum())
+
+    def focusinput(self):
+        self.cmdinput.setFocusPolicy(Qt.Qt.StrongFocus) 
+        self.cmdinput.setFocus()
 
     def setText(self, text):
         self.label.setText(text)
+    
+    def addText(self, text):
+        if self.label.text() != "":
+            self.label.setText(self.text() + '\n' + text)
+        else:
+            self.label.setText(text)
+            self.label.setHidden(False)
 
     def text(self):
         get_text = self.label.text()
         return get_text
 
 stylesheet = """
-    QScrollArea{border:none;border-bottom:1px solid #dadce0;}
+    QScrollArea{border:none;background:#000}
     
-    QScrollBar:vertical{background-color: #2A2949;width: 15px;margin: 15px 3px 15px 3px;border: 1px transparent #2A2929;border-radius: 4px;}
+    QScrollBar:vertical{background-color: rgb(43,45,50);width: 15px;margin: 15px 3px 15px 3px;border: 1px transparent #2A2929;border-radius: 4px;}
     QScrollBar::handle:vertical{background-color: #dadce0;min-height: 5px;border-radius: 4px;}
     QScrollBar::sub-line:vertical{margin: 3px 0px 3px 0px;height: 0px;width: 0px;subcontrol-position: top;subcontrol-origin: margin;}
     QScrollBar::add-line:vertical{margin: 3px 0px 3px 0px;height: 0px;width: 0px;subcontrol-position: bottom;subcontrol-origin: margin;}
@@ -47,7 +77,7 @@ stylesheet = """
     QScrollBar::up-arrow:vertical, QScrollBar::down-arrow:vertical{background: none;opacity: 0%;height: 0px;width:0px;}
     QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical{background: none;opacity: 0%;height: 0px;width:0px;}
     
-    QScrollBar:horizontal{background-color: #2A2949;width: 15px;margin: 15px 3px 15px 3px;border: 1px transparent #2A2929;border-radius: 4px;}
+    QScrollBar:horizontal{background-color:rgb(43,45,50);width:15px;margin: 15px 3px 15px 3px;border: 1px transparent #2A2929;border-radius: 4px;}
     QScrollBar::handle:horizontal{background-color: #dadce0;min-height: 5px;border-radius: 4px;}
     QScrollBar::sub-line:horizontal{margin: 3px 0px 3px 0px;height: 0px;width: 0px;subcontrol-position: top;subcontrol-origin: margin;}
     QScrollBar::add-line:horizontal{margin: 3px 0px 3px 0px;height: 0px;width: 0px;subcontrol-position: bottom;subcontrol-origin: margin;}
@@ -66,18 +96,19 @@ class MainWindow(QMainWindow):
         self.resize(720,500)
         self.setWindowIcon(QIcon("icon.png"))
         self.__tabwidget = QTabWidget()
-        self.__tabwidget.setStyleSheet("""QTabWidget::pane {border:0px;border-top: 1px solid lightgray;top:-1px;background: rgb(245, 245, 245);; } 
-QTabBar::tab {background: rgb(230, 230, 230); border: 1px solid lightgray; padding: 15px;} 
-QTabBar::tab:selected { background: rgb(245, 245, 245);}""")
+        self.__tabwidget.setStyleSheet("""QTabWidget::pane {border:0px;background: rgb(245, 245, 245);; } 
+QTabBar::tab {background: rgb(36,38,41);border:none;padding:20px;color:#AAA;border-top-right-radius:15px;border-top-left-radius:15px;}
+QTabBar::tab:hover {background:rgb(43,45,50);color:white;}
+QTabBar::tab:selected {background:rgb(43,45,50);color:white;}""")
         self.__main = MainTab(self)
-        self.__tabwidget.addTab(self.__main, "Servers")
+        self.setStyleSheet('QMainWindow{background:rgb(31,33,36)}')
+        self.__tabwidget.addTab(self.__main, "Home")
         vbox = QVBoxLayout()
         widget = QWidget()
         widget.setLayout(vbox)
         self.setCentralWidget(widget)
         vbox.setContentsMargins(0, 0, 0, 0)
         vbox.addWidget(self.__tabwidget)
-        self.setLayout(vbox)
     
     def NewTab(self, name:str, connection):
         tab = ServerTab(connection, self, name)
@@ -158,7 +189,7 @@ class MainTab(QMainWindow):
         QTableWidget::item:selected {background: rgb(66,70,77);color: #FFF;font-weight:999;border-radius:5px;} 
         QHeaderView::section{border-top:0px;border-left:0px;border-bottom: 2px solid rgb(43,45,50);padding:4px;}
         QTableCornerButton::section{border-top:0px solid #D8D8D8;border-left:0px solid #D8D8D8;border-bottom: 2px solid rgb(43,45,50);}
-        QLineEdit{border:none;border-radius:5px;background:#202225;font-size:12px;padding:5px;font-family:arial;color:#FFF;margin:3px;margin-top:0px}
+        QLineEdit{border:none;border-radius:5px;background:#202225;font-size:12px;padding:5px;font-family:verdana;color:#FFF;margin:3px;margin-top:0px}
         QPushButton{border-radius:5px;background:#202225;font-size:12px;font-family:arial;color:#FFF;padding:5px;margin:3px;margin-top:0px;padding-left:10px;padding-right:10px;}
         QLabel{font-size:12px;padding-left:20px;padding-bottom:5px;padding-top:10px;color:#FFF;font-weight:bold;font-family:verdana;border-top:2px solid rgb(43,45,50);}
         QScrollBar:vertical{background-color: rgb(34,37,43);width: 15px;margin: 15px 3px 15px 3px;border: 1px transparent;border-radius: 4px;}
@@ -290,87 +321,100 @@ class ServerTab(QMainWindow):
         grid = QGridLayout()
         grid.setContentsMargins(0, 0, 0, 0)
         self.__connection = connection
-        self.setStyleSheet('background-color:#19002c;color:white;padding:0;margin:0')
+        self.setStyleSheet('background:rgb(0,0,0);color:white;padding:0;margin:0')
         widget.setLayout(grid)
         self.__parent = parent
         self.__auth = False
-        self.__servername = servername
-        self.__cmdinput = QLineEdit('')
-        self.__cmdinput.setPlaceholderText('Type password . . .')
-        self.__cmdinput.setStyleSheet('border:none;margin:5px;margin-top:0px')
-        self.__cmdinput.editingFinished.connect(self.__send_message)
-        self.__cmdinput.setEchoMode(QLineEdit.Password)
+        self.servername = servername
         titlelabel = QLabel('Connection: ' + str(self.__connection.getpeername()[0]) + ':' + str(self.__connection.getpeername()[1]))
-        titlelabel.setStyleSheet('color:white;margin-left:5px;font-size:12px;font-family:verdana;')
+        titlelabel.setStyleSheet('color:white;margin-left:5px;font-size:12px;font-family:verdana;font-weight:bold')
         self.__exitbutton = QPushButton('𐌢')
+        self.__exitbutton.setFixedWidth(50)
         self.__exitbutton.setStyleSheet('QPushButton{background-color:transparent;padding:5px;border-radius:0px;color:#FFF;font-weight:100;font-family:arial;text-align:center;font-size:16px;}QPushButton:hover{background-color:#F02020;}QToolTip { background-color: black; color: white; border: black solid 1px}')
         self.__exitbutton.clicked.connect(self.__exit)
         self.__exitbutton.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
         self.__exitbutton.setToolTip('Disconnect from server')
-        self.__cmdlabel = ScrollLabel()
-        self.__cmdbutton = QPushButton('→')
-        self.__cmdbutton.setFixedWidth(50)
-        self.__cmdbutton.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-        self.__cmdbutton.setToolTip('Confirm')
-        self.__cmdbutton.setStyleSheet('QPushButton{border-radius:10px;margin:5px;margin-top:0;background-color:transparent;padding:5px;color:#FFF;font-weight:100;text-align:center;font-size:16px}QPushButton:hover{background-color:#FFF;color:black}QToolTip { background-color: black; color: white; border: black solid 1px}')
-        self.__cmdbutton.clicked.connect(self.__send_message)
+        self.cmdline = ScrollLabel(parent=self)
         grid.addWidget(titlelabel,0,0,1,2)
         grid.addWidget(self.__exitbutton,0,1)
-        grid.addWidget(self.__cmdlabel,1,0,1,2)
-        grid.addWidget(self.__cmdinput,2,0)
-        grid.addWidget(self.__cmdbutton,2,1)
+        grid.addWidget(self.cmdline,1,0,1,2)
+        self.cmdline.focusinput()
     
     
-    def authenticated(self):
-        self.__auth = True
-        self.__cmdinput.setPlaceholderText('Type command . . .')
-        self.__cmdinput.setEchoMode(QLineEdit.Normal)
-        self.__cmdlabel.setText('Established connection with ' + str(self.__connection.getpeername()[0]) + ':' + str(self.__connection.getpeername()[1]) + '\n')
+    def authenticated(self,state:bool=True):
+        if state:
+            self.__auth = True
+            self.cmdline.cmdinput.setPlaceholderText('Type command . . .')
+            self.cmdline.cmdinput.setEchoMode(QLineEdit.Normal)
+            self.clear()
+            self.cmdline.addText('Established connection with ' + str(self.__connection.getpeername()[0]) + ':' + str(self.__connection.getpeername()[1]))
+        else:
+            self.__auth = False
+            self.cmdline.cmdinput.setPlaceholderText('Type password . . .')
+            self.cmdline.cmdinput.setEchoMode(QLineEdit.Password)
     
     
-    def __scroll_to_bottom(self):
-        verScrollBar = self.__cmdlabel.verticalScrollBar()
-        verScrollBar.setValue(verScrollBar.maximum())
+    def new_connection(self,socket):
+        self.__connection = socket
     
+    '''
+    def graph(self):
+        while self.show_grap:
+            #time.sleep(5)
+            #show the graph element
+            #self.__connection.send('<cpu_graph_percent>'.encode()) <-- this would return to the client a value of <int:cpupercent>
     
-    def __send_message(self):
-        command = self.__cmdinput.text()
+    def print_graph(self,x,y):
+        ###THIS FUNCTION WILL BE EXECUTED FROM THE LISTEN SERVER THREAD.
+        #maximum of 50 values on display at once with a delay of 5 seconds each.
+        
+    '''
+    
+    def send_message(self):
+        command = self.cmdline.cmdinput.text()
         if len(command) > 0:
             if self.__auth and (command.lower() == 'clear' or command.lower() == 'cls'):
-                self.__cmdinput.setText('')
-                self.__cmdlabel.setText('')
+                self.cmdline.cmdinput.setText('')
+                self.clear()
             elif self.__auth and command.lower().split(' ')[0] == 'rename':
-                self.__cmdlabel.setText(self.__cmdlabel.text() + self.__servername + ' > ' + command + '\n')
-                self.__servername = command.split(' ', 1)[1]
-                self.__parent.renameTab(self.__servername)
-                self.__cmdlabel.setText(self.__cmdlabel.text() + f'Changed name to {self.__servername}\n')
-                self.__cmdinput.setText('')
+                self.cmdline.addText(self.servername + ' > ' + command)
+                self.servername = command.split(' ', 1)[1]
+                self.__parent.renameTab(self.servername)
+                self.cmdline.username.setText(self.servername + ' >')
+                self.cmdline.addText(f'Changed name to {self.servername}')
+                self.cmdline.cmdinput.setText('')
             else:
                 try:
-                    self.__cmdinput.setText('')
+                    self.cmdline.cmdinput.setText('')
                     if self.__auth:
-                        self.__cmdlabel.setText(self.__cmdlabel.text() + self.__servername + ' > ' + command + '\n')
+                        self.cmdline.addText(self.servername + ' >  ' + command)
                         self.__connection.send(('$' + command).encode())
                     else:
                         self.__connection.send(command.encode())
                 except:
-                    self.__cmdlabel.setText(self.__cmdlabel.text() + 'Conection was lost. \n')
-            self.__scroll_to_bottom()
+                    self.cmdline.addText('Conection was lost.')
     
     
     def response(self, data):
-        self.__cmdlabel.setText(self.__cmdlabel.text() + data + '\n')
-        self.__cmdlabel.verticalScrollBar().setValue(self.__cmdlabel.verticalScrollBar().maximum())
-        self.__scroll_to_bottom()
+        self.cmdline.addText(data)
     
+    def clear(self):
+        self.cmdline.setText('')
+        self.cmdline.label.setHidden(True)
     
     def closeTab(self):
         self.deleteLater()
     
     
     def __exit(self):
-        self.__connection.close()
-        self.deleteLater()
+        try:
+            if self.__auth:
+                self.__connection.send('$exit'.encode())
+            else:
+                self.__connection.send('exit'.encode())
+        except:
+            self.__connection.close()
+            self.closeTab()
 
 
 #________
@@ -378,6 +422,19 @@ class ServerTab(QMainWindow):
 
 def disconnect(id:str):
     sockets[id].close()
+
+
+def attempt_reconnect(address):
+    for i in range(0,5):
+        client_socket = socket.socket()
+        try:
+            client_socket.connect((address[0],address[1]))
+            client_socket.setblocking(0)
+            client_socket.send('<testing_connection>'.encode())
+            return client_socket
+        except:
+            time.sleep(1)
+    return None
 
 
 def listen(client, connection_id:str):
@@ -389,7 +446,7 @@ def listen(client, connection_id:str):
             client[0].close()
             sys.exit()
         try:
-            data = client[0].recv(1024)
+            data = client[0].recv(2048)
             if not data:
                 pass
             else:
@@ -400,6 +457,19 @@ def listen(client, connection_id:str):
                     client[1].authenticated()
                 if cleaned == 'closing socket':
                     force_stopped_threads.append(connection_id)
+                elif cleaned == 'resetting':
+                    address = client[0].getpeername()
+                    client[0].close()
+                    auth = False
+                    client[1].clear()
+                    client[1].response('reconnecting...')
+                    client[0] = attempt_reconnect(address)
+                    if client[0] != None:
+                        client[1].new_connection(client[0])
+                        client[1].authenticated(False)
+                    else:
+                        client[1].response('Failed to reconnect.')
+                        client[1].closeTab()
         except:
             pass
 
